2f250472208dfc28580c2530de988d68a4b07c0f
Devil月哥 <yuex.s@xd5u.com>
2018-03-31 21:44:19 +0800

    添加项目文件。

---
+++ src/PhoneBookProject.Application/Sessions/SessionAppService.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Abp.Auditing;
+using PhoneBookProject.Sessions.Dto;
+using PhoneBookProject.SignalR;
+
+namespace PhoneBookProject.Sessions
+{
+    public class SessionAppService : PhoneBookProjectAppServiceBase, ISessionAppService
+    {
+        [DisableAuditing]
+        public async Task<GetCurrentLoginInformationsOutput> GetCurrentLoginInformations()
+        {
+            var output = new GetCurrentLoginInformationsOutput
+            {
+                Application = new ApplicationInfoDto
+                {
+                    Version = AppVersionHelper.Version,
+                    ReleaseDate = AppVersionHelper.ReleaseDate,
+                    Features = new Dictionary<string, bool>
+                    {
+                        { "SignalR", SignalRFeature.IsAvailable },
+                        { "SignalR.AspNetCore", SignalRFeature.IsAspNetCore }
+                    }
+                }
+            };
+
+            if (AbpSession.TenantId.HasValue)
+            {
+                output.Tenant = ObjectMapper.Map<TenantLoginInfoDto>(await GetCurrentTenantAsync());
+            }
+
+            if (AbpSession.UserId.HasValue)
+            {
+                output.User = ObjectMapper.Map<UserLoginInfoDto>(await GetCurrentUserAsync());
+            }
+
+            return output;
+        }
+    }
+}
